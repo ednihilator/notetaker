c86b19abec64ca2bc901a0b27dd12e57163ddb93
ednihilator <boyofpopsicle@gmail.com>
2023-08-02 15:56:16 -0400

    finished the readme

--- db/store.js
+++ db/store.js
@@ -1,5 +1,6 @@
 const util = require("util");
 const fs = require("fs");
+const { v4: newID } = require("uuid");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
@@ -9,12 +10,38 @@ class Store {
   }
 
   write(note) {
-    return writeFileAsync("db/db.json", note);
+    return writeFileAsync("db/db.json", JSON.stringify(note));
   }
   getNotes() {
     return this.read().then((result) => {
       console.log(typeof result, result.length);
       return JSON.parse(result);
+    });
+  }
+
+  postNotes(note) {
+    //read
+    return this.getNotes()
+      .then((existingNotes) => {
+        note.id = newID();
+        //modify
+        existingNotes.push(note);
+        //write
+        return existingNotes;
+      })
+      .then((newNotes) => {
+        this.write(newNotes);
+      })
+      .then(() => {
+        return note;
+      });
+  }
+  deleteNote(noteId) {
+    return this.getNotes().then((existingNotes) => {
+      const filteredNotes = existingNotes.filter((note) => {
+        return note.id !== noteId;
+      });
+      this.write(filteredNotes);
     });
   }
 }
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -11,5 +11,25 @@ router.get("/notes", (req, res) => {
       return res.status(500).json(err);
     });
 });
+router.post("/notes", (req, res) => {
+  store
+    .postNotes(req.body)
+    .then((newNote) => {
+      return res.json(newNote);
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
+});
+router.delete("/notes/:id", (req, res) => {
+  store
+    .deleteNote(req.params.id)
+    .then(() => {
+      res.json({ response: "deleted note id:" + req.params.id });
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
+});
 
 module.exports = router;
